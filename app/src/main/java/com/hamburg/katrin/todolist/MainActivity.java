e4b823cfb938316e9d56ea9e2a33a483f79b8dd5
Katrin 3caragiu <3caragiu@informatik.uni-hamburg.de>
2017-07-16 22:34:38 +0200

    Added a Listener to open the Item List.

--- app/src/main/java/com/hamburg/katrin/todolist/MainActivity.java
+++ app/src/main/java/com/hamburg/katrin/todolist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hamburg.katrin.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+                                      public void onClick(View arg0) {
+                                          Intent viewIntent = new Intent(MainActivity.this, NewListActivity.class);
+                                          startActivity(viewIntent);
+                                      }
+                                  }
+            );
     }
+
+    /*---unnötig geworden durch den Listener: ---*/
+   /* public void createNewList(View view) { //import with alt + enter
+        Intent intent = new Intent(MainActivity.this, NewListActivity.class);
+        startActivity(intent);
+    }
+    */
+
+
 }
---
+++ app/src/main/java/com/hamburg/katrin/todolist/NewListActivity.java
@@ -0,0 +1,18 @@
+package com.hamburg.katrin.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+
+public class NewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_list);
+
+    }
+}
